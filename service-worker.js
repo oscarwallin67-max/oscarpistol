2ec395c7c6acce091597eaad51bce04db0a2570b
oscarwallin67-max <oscar.wallin67@gmail.com>
2025-11-27 16:15:17 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,43 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'pistolapp-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/pistol6.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+// Install SW + cache all files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate SW + clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch from cache first
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
