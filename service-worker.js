76648752fc15fec24085ca78589567d1781d73a8
oscarwallin67-max <oscar.wallin67@gmail.com>
2025-11-28 11:52:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,36 @@
-const CACHE_NAME = "pistolapp-v10";
+const CACHE_NAME = "pistolapp-v11";
 
 const urlsToCache = [
-  "/oscarpistol/",
-  "/oscarpistol/index.html",
-  "/oscarpistol/pistol6.html",
-  "/oscarpistol/manifest.json",
-  "/oscarpistol/icon.png",
-  "/oscarpistol/service-worker.js"
+  "./",
+  "./index.html",
+  "./pistol6.html",
+  "./manifest.json",
+  "./icon.png"
 ];
 
 // Install
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
 // Activate
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null))
-      )
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
     )
   );
   self.clients.claim();
 });
 
 // Fetch
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(
-      resp => resp || fetch(event.request)
-    )
+    caches.match(event.request, { ignoreSearch: true }).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
